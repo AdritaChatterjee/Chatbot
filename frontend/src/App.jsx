e5d24108cd5a572c1a151edc87e352e6e125fa34
Sudeshna <sudeshnakundu2004@gmail.com>
2024-09-15 20:52:01 +0530

    error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,7 @@ import Right from './right/right';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <div className="flex">
-=======
     <div className="flex h-screen">
->>>>>>> 0ae7d917a5d9338ff0547c144f9f59098aade5df
     <Left></Left>
     <Right></Right>
     </div>
