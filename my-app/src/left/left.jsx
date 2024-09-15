d7c443f5d0d13c72693729177092d983aceff568
Sudeshna <sudeshnakundu2004@gmail.com>
2024-09-15 18:47:46 +0530

    Left Right

---
+++ my-app/src/left/left.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+function left(){
+    return <div style={{ border: '2px solid black' }}>Left</div>;
+}
+
+export default left;
---
+++ my-app/src/right/right.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+function right(){
+    return<div style="border border-black">Right</div>;
+}
+export default right;
